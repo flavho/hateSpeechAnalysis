12c0d2e21b144f20d72b73af80f2629c6d8eab90
flavho <58787542+flavho@users.noreply.github.com>
2020-06-09 22:37:17 +0200

    English CSV Import Added + Filtering the Stopwords

--- src/mainHandler.py
+++ src/mainHandler.py
@@ -1,5 +1,9 @@
 import csv
-
+import nltk
+from nltk.tokenize import word_tokenize
+# nltk.download() #if stopwords not downloaded: Coropora -> Stopwords -> download
+stopwordsDe = nltk.corpus.stopwords.words('german')
+stopwordsEn = nltk.corpus.stopwords.words('english')
 
 allTweets = []
 
@@ -8,14 +12,35 @@ class Tweet:
     self.tweet = tweet
     self.hatespeech = hatespeech
 
-def loadCsvToTweetObject():
-    with open('C:/Users/flavi/Desktop/NLP/germanTweets.csv', newline='') as csvfile:
-        tweetreader = csv.reader(csvfile, delimiter=',')
-        for row in tweetreader:
-            tweetText = row[0]
-            item = Tweet(tweetText, False)
-            allTweets.append(item)
-            
-            
+def loadGermanCsvToTweetObject():
+  with open('C:/Users/flavi/Desktop/hateSpeechAnalysis-master/germanTweets.csv', encoding="utf8", newline='') as csvfile:
+      tweetreader = csv.reader(csvfile, delimiter=',')
+      for row in tweetreader:
+          tweetText = row[0]
+          item = Tweet(tweetText, False)
+          allTweets.append(item)
 
-loadCsvToTweetObject()
+def loadEnglishCsvToTweetObject():   
+  with open('C:/Users/flavi/Desktop/hateSpeechAnalysis-master/englishTweets.csv', encoding="utf8", newline='') as csvfile:
+      tweetreader = csv.reader(csvfile, delimiter=';')
+      for row in tweetreader:
+          tweetText = row[1]
+          item = Tweet(tweetText, False)
+          allTweets.append(item)
+
+def removeStopWords(stopwords): 
+  for tweet in allTweets:
+    text = tweet.tweet
+    word_tokens = word_tokenize(text) 
+    filtered_sentence = [w for w in word_tokens if not w in stopwords] 
+    filtered_sentence = [] 
+    for w in word_tokens: 
+      if w not in stopwords: 
+          filtered_sentence.append(w) 
+    print(word_tokens) 
+    print(filtered_sentence) 
+
+
+#loadEnglishCsvToTweetObject()
+loadGermanCsvToTweetObject()
+removeStopWords(stopwordsDe)
