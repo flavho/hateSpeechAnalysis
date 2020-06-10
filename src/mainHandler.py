a63cb42147cfedf8594b274ba99808880a849b21
flavho <58787542+flavho@users.noreply.github.com>
2020-06-10 12:48:33 +0200

    Update mainHandler.py
    tweets are stored in allTweets[] by removeStopWords() without stopwords and tokenized

--- src/mainHandler.py
+++ src/mainHandler.py
@@ -7,6 +7,7 @@ stopwordsDe = nltk.corpus.stopwords.words('german')
 stopwordsEn = nltk.corpus.stopwords.words('english')
 myPath = os.path.abspath(os.path.dirname(__file__))
 allTweets = []
+removeStopWordsLoopCounter = 0
 
 class Tweet:
   def __init__(self, tweet, hatespeech):
@@ -32,19 +33,20 @@ def loadEnglishCsvToTweetObject():
           allTweets.append(item)
 
 def removeStopWords(stopwords): 
-  for tweet in allTweets:
-    text = tweet.tweet
+  #after calling this function it will put the tokenized Text without Stopwords in the fitting Tweet Object
+  for i in range(len(allTweets)):
+    text = allTweets[i].tweet
     word_tokens = word_tokenize(text) 
     filtered_sentence = [w for w in word_tokens if not w in stopwords] 
     filtered_sentence = [] 
     for w in word_tokens: 
       if w not in stopwords: 
-          filtered_sentence.append(w) 
-    print(word_tokens) 
-    print(filtered_sentence) 
+          filtered_sentence.append(w)
+    allTweets[i].tweet = filtered_sentence
 
 
-loadEnglishCsvToTweetObject()
-#loadGermanCsvToTweetObject()
-#removeStopWords(stopwordsDe)
-removeStopWords(stopwordsEn)
+    
+#loadEnglishCsvToTweetObject()
+loadGermanCsvToTweetObject()
+removeStopWords(stopwordsDe)
+#removeStopWords(stopwordsEn)
