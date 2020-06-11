3d995ec20b7d0b5f9ee8e23786cd2060a3106f16
flavho <58787542+flavho@users.noreply.github.com>
2020-06-12 01:09:53 +0200

    Save all Tweets completly formated to Array. TODO: Umlaute spacken ab

--- src/mainHandler.py
+++ src/mainHandler.py
@@ -1,8 +1,10 @@
 import csv
 import nltk
 import os 
+import html
+import re
 from nltk.tokenize import word_tokenize
-# nltk.download() #if stopwords not downloaded: Coropora -> Stopwords -> download
+#nltk.download() #if stopwords not downloaded: Coropora -> Stopwords -> download
 stopwordsDe = nltk.corpus.stopwords.words('german')
 stopwordsEn = nltk.corpus.stopwords.words('english')
 myPath = os.path.abspath(os.path.dirname(__file__))
@@ -32,21 +34,58 @@ def loadEnglishCsvToTweetObject():
           item = Tweet(tweetText, False)
           allTweets.append(item)
 
-def removeStopWords(stopwords): 
+def removeStopWordsAndTokenize(stopwords, tweetText): 
   #after calling this function it will put the tokenized Text without Stopwords in the fitting Tweet Object
-  for i in range(len(allTweets)):
-    text = allTweets[i].tweet
-    word_tokens = word_tokenize(text) 
+    word_tokens = word_tokenize(tweetText) 
     filtered_sentence = [w for w in word_tokens if not w in stopwords] 
     filtered_sentence = [] 
     for w in word_tokens: 
       if w not in stopwords: 
           filtered_sentence.append(w)
-    allTweets[i].tweet = filtered_sentence
+    return filtered_sentence
 
+def lowerString(tweetText):
+  # all Strings to lower case
+  return tweetText.lower()
 
+def convertHtml(tweetText):
+  # Convert html to Unicode
+  return html.unescape(tweetText)
+
+def adressParsing(tweetText):
+  # wwww and https to ""
+  return re.sub('((www\.[^\s]+)|(https?://[^\s]+))','',tweetText)
     
-#loadEnglishCsvToTweetObject()
-loadGermanCsvToTweetObject()
-removeStopWords(stopwordsDe)
-#removeStopWords(stopwordsEn)
+def usernameToEmptyString(tweetText):
+  #username to emtpy String
+  return re.sub('@[^\s]+','',tweetText)
+    
+def removeWhiteSpaces(tweetText):
+  #Remove all white spaces
+  return re.sub('[\s]+', ' ', tweetText)
+  
+def removeHashtags(tweetText):
+  #Replace # with ""
+  return re.sub(r'#([^\s]+)', r'\1', tweetText)
+
+def trimString(tweetText):
+  # removes Spaces at start / end
+  return tweetText.strip('\'"')
+
+def removeNonChars(tweetText):
+  # remove all characters that are not in the alphabet
+  return re.sub('[^A-Za-z0-9 ]+','', tweetText)
+
+def formatAllTweetsforNltkDe():
+  # takes all Tweets from csv and puts them in Array after they got formated and tokenized
+  for i in range(len(allTweets)):
+    text = allTweets[i].tweet
+    toTokenize = lowerString(convertHtml(adressParsing(usernameToEmptyString(removeWhiteSpaces(removeHashtags(trimString(removeNonChars(text))))))))
+    print(removeStopWordsAndTokenize(stopwordsDe, toTokenize))
+    allTweets[i].tweet = removeStopWordsAndTokenize(stopwordsDe, toTokenize)
+
+def main():
+  loadGermanCsvToTweetObject()
+  formatAllTweetsforNltkDe()
+
+main()
